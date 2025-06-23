6663276e6272d64733d75cff94e0d258c1262ca4
BytesPlatform <developer@bytesplatform.com>
2025-06-23 14:00:26 -0700

    Changing api routes

--- client/src/components/ImageCarousel3D.jsx
+++ client/src/components/ImageCarousel3D.jsx
@@ -6,14 +6,14 @@ const ImageCarousel3D = () => {
   const images = [
     '/uploads/6-gallery-wedding-aerial.jpg',
     '/uploads/1-gallery-vip-arrival.jpg',
-    '/uploads/12-gallery-bell-hughes.jpg',
+    '/uploads/2-gallery-bell-hughes.jpg',
     '/uploads/3-gallery-raptor-atv.jpg',
     '/uploads/4-gallery-oahu-magnum.jpg',
-    '/uploads/5-gallery-wedding-photo.jpg',
+    '/uploads/14-gallery-wedding-photo.jpg',
     '/uploads/7-gallery-hawaiian-landscape.jpg',
-    '/uploads/8-gallery-coastal-ops.jpg',
-    '/uploads/9-gallery-island-aviation.jpg',
-    '/uploads/10-gallery-horseback-sky.jpg',
+    '/uploads/13-gallery-coastal-ops.jpg',
+    '/uploads/12-gallery-island-aviation.jpg',
+    '/uploads/11-gallery-horseback-sky.jpg',
   ];
 
   return (
